f53d79ee270953723e449509a0dda5de0c25992f
kmstumpff <kmstumpff@gmail.com>
2025-04-07 23:04:11 -0400

    fix entity importing, add AirOSStatus export

--- pyairos/__init__.py
+++ pyairos/__init__.py
@@ -1,6 +1,1 @@
-# from .session import Session
-# from .authorization import AuthorizationApi
-# from .base import BaseApi
-# from .status import StatusApi
-
-from .airosapi import AirOSApi
+from .airosapi import AirOSApi, AirOSStatus
